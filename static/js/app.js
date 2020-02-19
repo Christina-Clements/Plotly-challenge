3758b23f77af4e2b4951820c3e1268d09698551b
Christina Clements <crfc.tk@gmail.com>
2020-02-19 16:55:01 -0600

    2nd attempt

--- static/js/app.js
+++ static/js/app.js
@@ -4,7 +4,7 @@ function handleSubmit() {
   d3.event.preventDefault();
   var sample_values= d3.select("#sample-metadata").node().value;
   console.log(sample);
-  d3.select("#stockInput").node().value = "";
+  d3.select("#selDataset").node().value = "";
   buildMetaData(sample);
 }
 function buildMetadata(sample) { 
@@ -20,8 +20,10 @@ function buildMetadata(sample) {
     var wfreq = sample.metadata.wfreq;
 
     var trace1 = {
-      x: otu_ids,
-      y: sample_values,
+      x: otu_ids.map(row => row.pair),
+      y: sample_values.map(row => row.wfreq),
+      text: sample_values.map(row => row.name),
+      name: "OTU IDs",
       type: "bar",
       mode: "horizontal"
       };
@@ -30,24 +32,6 @@ function buildMetadata(sample) {
         xaxis: { title: "OTU ID"}
       };
     Plotly.newPlot("bubble", bubbledata, layout);
-    d3.json(url).then((data)=>{
-      var sValues = data.sample_values.slice(0,9);
-      var sLabels = data.otu_ids.slice(0,9);
-      var sText = data.otu_labels.slice(0,9);
-      var trace2 = {
-        labels : sLabels,
-        values : sValues,
-        hovertext : sText,
-        hoverinfo: "hovertext",
-        type: "pie" 
-      };
-      var data2 = [trace2];
-      var layout2 = {
-        title: "Top 10 Samples",
-        autosize: true
-      };
-      Plotly.newPlot("pie", data2, layout2);
-    });
   });
   d3.select("#submit").on("click", handleSubmit);
 };
@@ -56,42 +40,42 @@ function buildMetadata(sample) {
     // Make an API call to gather all data and then reduce to matching the sample selected
     //TODO: 
   
-  d3.json(URL).then((sample) =>{
-    var panel = d3.select("#sample-metadata");
-    panel.html("");
-    console.log(data);
+//   d3.json(URL).then((sample) =>{
+//     var panel = d3.select("#sample-metadata");
+//     panel.html("");
+//     console.log(data);
       
-    Object.entries(sample).forEach(([key, value]) => {
-      var paragraph = panel.append("p");
-      paragraph.text(`${key} : ${value}`);
-    });
-  });
+//     Object.entries(sample).forEach(([key, value]) => {
+//       var paragraph = panel.append("p");
+//       paragraph.text(`${key} : ${value}`);
+//     });
+//   });
 
 function init() {
     
-    // Grab a reference to the dropdown select element
+//     // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
     
     // Use the list of sample names to populate the select options
-    // d3.json("samples.json").then((data) => {
-    //   var sampleNames = data.names;
+  d3.json("samples.json").then((data) => {
+    var sampleNames = data.names;
       
-    //   // Use the first sample from the list to build the initial plots
-    //   var firstSample = sampleNames[0];
-    //   buildCharts(firstSample);
-    //   buildMetadata(firstSample);
-    d3.json("names").then((sampleNames) => {
-      sampleNames.forEach((sample) => {
-        selector
-          .append("option")
-          .text(sample)
-          .property("value", sample);
-      });
-      const firstSample = sampleNames[0];
+      // Use the first sample from the list to build the initial plots
+    var firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+  d3.json("names").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    const firstSample = sampleNames[0];
       buildCharts(firstSample);
       buildMetadata(firstSample);
     });
-}
+  });
 
 // function optionChanged(newSample) {
 //       // Loop through sampleNames to add "option" elements to the selector
@@ -105,7 +89,8 @@ function init() {
     // Fetch new data each time a new sample is selected
     buildCharts(newSample);
     buildMetadata(newSample);
-  }
+  };
   
   // Initialize the dashboard
-  init();
+init()
+}
