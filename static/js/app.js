9ae4a3e96f7e73514d1f00a7457c1ccb917056f0
Christina Clements <crfc.tk@gmail.com>
2020-03-24 20:35:20 -0500

    plotly revamp

--- static/js/app.js
+++ static/js/app.js
@@ -1,99 +1,77 @@
+
+    // Make an API call to gather all data and then reduce to matching the sample selected
+    //TODO: 
+
 function init() {
-  sample = [{
-    x: [1,2,3],
-    y: [2,4,6]}];
-  Plotly.newPlot("plot", data);
-}
-var url = `http://robdunnlab.com/projects/belly-button-biodiversity/`;
-function handleSubmit() {
-    // Make an API call to gather all data and then reduce to matching the sample selected
-    //TODO: 
-  d3.event.preventDefault();
-  var sample_values= d3.select("#sample-metadata").node().value;
-  console.log(sample);
-  d3.select("#selDataset").node().value = "";
-  buildMetaData(sample);
-}
-function buildMetadata(sample) { 
-  d3.json(url).then((sample) => {
-    var name = sample.names;
-    var otu_ids = sample.metadata.id;
-    var ethnicity = sample.metadata.ethnicity;
-    var gender = sample.metadata.gender;
-    var age = sample.metadata.age;
-    var location = sample.metadata.location;
-    var bbtype = sample.metadata.bbtype;
-    var wfreq = sample.metadata.wfreq;
-
-    var trace1 = {
-      x: otu_ids.map(row => row.pair),
-      y: sample_values.map(row => row.wfreq),
-      text: sample_values.map(row => row.name),
-      name: "OTU IDs",
-      type: "bar",
-      mode: "horizontal"
-      };
-    var bubbledata = [trace1];
-    var layout = {
-        xaxis: { title: "OTU ID"}
-      };
-    Plotly.newPlot("bubble", bubbledata, layout);
-  });
-  d3.select("#submit").on("click", handleSubmit);
-};
-
-
-    // Make an API call to gather all data and then reduce to matching the sample selected
-    //TODO: 
-  
-  d3.json(URL).then((sample) =>{
-    var panel = d3.select("#sample-metadata");
-    panel.html("");
-    console.log(data);
-      
-    Object.entries(sample).forEach(([key, value]) => {
-      var paragraph = panel.append("p");
-      paragraph.text(`${key} : ${value}`);
-    });
-  });
     
-//     // Grab a reference to the dropdown select element
+    // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
-  Plotly.newPlot("plot", data);
+    
     // Use the list of sample names to populate the select options
   d3.json("samples.json").then((data) => {
     var sampleNames = data.names;
       
-      // Use the first sample from the list to build the initial plots
-    var firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
-  });
-  d3.json("names").then((sampleNames) => {
     sampleNames.forEach((sample) => {
       selector
         .append("option")
         .text(sample)
         .property("value", sample);
-    const firstSample = sampleNames[0];
+      
+
+      // Use the first sample from the list to build the initial plots
+    var firstSample = sampleNames[0];
       buildCharts(firstSample);
       buildMetadata(firstSample);
     });
   });
+}
 
-// function optionChanged(newSample) {
-//       // Loop through sampleNames to add "option" elements to the selector
-//       //TODO: 
+var url = "samples.json";
+function buildMetadata(sample) {
+  d3.json(url).then((data) => {
+    console.log(data);
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(metadataSample => metadataSample.id == sample);
+    var result = resultArray[0];
+    var panel = d3.select("#sample-metadata");
+    panel.html("");
 
+    Object.entries(result).forEach(([key, value]) => {
+      var paragraph = panel.append("p");
+      paragraph.text(`${key} : ${value}`);
+    });
+  });
+}
 
-//   };
+function buildCharts(sample) {
+  d3.json(url).then((data) =>{
+    console.log(data)
+    var samples = data.samples;
+    var resultArray = samples.filter(samplesSample => samplesSample.id == sample);
+    var otuSample = resultArray[0];
+    var otu_ids = otuSample.otu_ids;
+    var sample_values = otuSample.sample_values;
+    var otu_labels = otuSample.otu_labels;
+    var bubbleTrace = {
+      x: otu_ids, 
+      y: sample_values,
+      text: otu_labels,
+      name: "OTU IDs",
+      mode: "markers"
+      };
+    var bubbleData = [bubbleTrace];
+    var layout ={
+      xaxis: {title: "OTU ID"}
+      };
+    Plotly.newPlot("bubble", bubbleData, layout);
+  });
+};
   
-  
-  function optionChanged(newSample) {
+function optionChanged(newSample) {
     // Fetch new data each time a new sample is selected
-    buildCharts(newSample);
-    buildMetadata(newSample);
-  };
+  buildCharts(newSample);
+  buildMetadata(newSample);
+};
   
   // Initialize the dashboard
-init()
+init();
